ca69d2bc57b31696c22a8c50ffabf4af0ed3a386
Adrien RICHARD <richardadrien0@gmail.com>
2022-03-29 15:00:07 +0200

    lots of mods

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -1,32 +1,27 @@
 import csv
 import json
 from pathlib import Path
-
+import time
 import paho.mqtt.client as MQTTLib
 from typer import Typer
 
 cli = Typer()
-
 
 def on_connect(client, userdata, flags, rc):
     print(userdata)
     print(flags)
     print("Connection returned result: " + MQTTLib.connack_string(rc))
 
-
 def etablish_mqtt_connexion():
     client = MQTTLib.Client()
     client.on_connect = on_connect
     client.connect("iamosquitto", 10083, 60)
 
-    yield client
-
+    return client
 
 def publish(*, topic: str, payload: str):
-
-    with etablish_mqtt_connexion() as client:
-        client.publish(topic, payloads)
-
+    client = etablish_mqtt_connexion()
+    client.publish(topic, payload)
 
 def create_csv_file(header: str, d: list, index: int):
     if not d:
@@ -36,11 +31,10 @@ def create_csv_file(header: str, d: list, index: int):
     for row in d:
         data.append(",".join(row))
 
-    with open(f"debug/data/{index}.csv", "w") as f:
+    with open(f"/data/{index}.csv", "w") as f:
         f.writelines(",".join(header))
         f.write("\n")
         f.writelines("\n".join(data))
-
 
 @cli.command()
 def split_file():
@@ -49,7 +43,7 @@ def split_file():
     line_numbers = 0
     header = []
 
-    with open("debug/data/dataset_MP.csv") as f:
+    with open("/data/dataset_MP.csv") as f:
 
         list_data = list(csv.reader(f, delimiter=","))
         line_numbers = len(list_data)
@@ -64,13 +58,14 @@ def split_file():
 
 @cli.command()
 def file_to_mqtt():
-    path = Path("debug/data")
+    path = Path("/data")
 
     for file in path.iterdir():
         if file.name != "dataset_MP.csv":
             content = file.read_text()
+            print(f"sensor/{file.stem}")
             publish(topic=f"sensor/{file.stem}", payload=json.dumps(content.split("\n").pop(0)))
-
+            time.sleep(60)
 
 if __name__ == "__main__":
     cli()
