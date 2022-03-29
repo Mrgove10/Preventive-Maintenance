8765f68891617971649d23c8bad90a4460a3b51e
Adrien RICHARD <richardadrien0@gmail.com>
2022-03-29 14:02:54 +0200

    mods

--- insert/app.js
+++ insert/app.js
@@ -1,5 +1,7 @@
 const mqtt = require('mqtt')
-const client  = mqtt.connect('mqtt://iamosquitto:10083')
+const client = mqtt.connect('mqtt://iamosquitto:10083')
+const { InfluxDB, Point } = require("@influxdata/influxdb-client");
+
 
 client.on('connect', function () {
   client.subscribe('sensor/#', function (err) {
@@ -16,4 +18,32 @@ client.on('message', function (topic, message) {
 })
 setInterval(() => {
   console.log("COUCOU")
-}, 25000);
+}, 2500);
+
+
+setTimeout(() => {
+  // const url = "https://us-west-2-1.aws.cloud2.influxdata.com";
+  const url = "http://iainflux:10086";
+  const token = "my-token";
+  const org = "my-org";
+  const bucket = "my-bucket";
+
+  const influxclient = new InfluxDB({ url: url, token: token });
+  const writeApi = influxclient.getWriteApi(org, bucket);
+  const point = new Point("weatherstation")
+    .tag("sensor", 1)
+    .floatField("temperature", 23.4)
+    .timestamp(new Date());
+
+  writeApi.writePoint(point);
+
+  writeApi
+    .close()
+    .then(() => {
+      console.log("FINISHED");
+    })
+    .catch((e) => {
+      console.error(e);
+      console.log("Finished ERROR");
+    });
+}, 30000);
